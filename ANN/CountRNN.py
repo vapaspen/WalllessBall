cf431c30121eee0486b85d6534bb0ca671d0eb7a
vapaspen <vapaspen@gmail.com>
2016-03-12 23:00:07 -0700

    Added RNN

--- ANN/CountRNN.py
+++ ANN/CountRNN.py
@@ -8,9 +8,10 @@ import numpy as np
 class Experimental_ANN:
 
     def __init__(self, X, H, O, learning_rate=0.1):
-        self.H = rl.ANN_Layer(X, H)
-        self.OL = rl.ANN_Layer(H, O)
-        self.OR = rl.ANN_Layer(H, O)
+        self.H = rl.ANN_Layer(X, H, is_recurrent=True)
+        self.OL = rl.ANN_Layer(H, O, is_recurrent=True)
+        self.OR = rl.ANN_Layer(H, O, is_recurrent=True)
+
         self.learning_rate = learning_rate
         self.errorL = 0
         self.errorR = 0
@@ -18,16 +19,19 @@ class Experimental_ANN:
     def feed_foward(self, input_data):
         H = self.H.feed_foward(input_data)
 
+        OR = self.OR.feed_foward(H)
 
-        return [self.OL.feed_foward(H),self.OR.feed_foward(H)]
+
+
+        return [self.OL.feed_foward(H),OR]
 
     def train_on_data(self, data, targetL, targetR):
         this_pass = self.feed_foward(data)
         self.errorL = this_pass[0] - targetL
         self.errorR = this_pass[1] - targetR
 
-        o_error = self.OR.backpropagate(self.errorR, learning_rate=self.learning_rate) + self.OL.backpropagate(self.errorL, learning_rate=self.learning_rate)
-        h_error = self.H.backpropagate(o_error, learning_rate=self.learning_rate)
+        h_error = self.OR.backpropagate(self.errorR, learning_rate=self.learning_rate) + self.OL.backpropagate(self.errorL, learning_rate=self.learning_rate)
+        self.H.backpropagate(h_error, learning_rate=self.learning_rate)
 
 
 
@@ -38,7 +42,7 @@ xor_data = [[zero, zero], [zero, one], [one, zero], [one, one]]
 xor_data_trainL = [one, zero, zero, one]
 xor_data_trainR = [zero, one, one, zero]
 
-NN = Experimental_ANN(2, 4, 1)
+NN = Experimental_ANN(2, 3, 1)
 
 test_error = .0001
 
@@ -48,14 +52,10 @@ while run:
     #for i in range(4):
     i = random.randrange(4)
     NN.train_on_data(xor_data[i], xor_data_trainL[i], xor_data_trainR[i])
-    test_error = (NN.errorL + NN.errorR + test_error) /2
+    test_error = (NN.errorL  + NN.errorR + test_error) /2
     if n % 1000 == 0:
-        print("Trial: " + str(n) + " Index: " + str(i) + " Error: " + str(test_error) + " Result: " + str(NN.OL.Layer["nodes"]["neurons"]) + " "+ str(NN.OR.Layer["nodes"]["neurons"]) + " Expected: " + str(xor_data_trainL[i]) + ", " + str(xor_data_trainR[i]))
-        #print("self.delta_del_delta_hidden_bias: " + str(np.mean(NN.delta_del_delta_hidden_bias)))
-        #print("self.delta_del_hidden_bias: " + str(NN.delta_del_hidden_bias))
-        if i == 1:
-            #print("Index: " + str(i) + " Error: " + str(NN.error) + " Result: " + str(NN.output_layer) + " Expected: " + str(xor_data_train[i]))
-            pass
+        print("Trial: " + str(n) + " Index: " + str(i) + " Error: " + str(test_error) + " Result: Left: " + str(NN.OL.Layer["nodes"]["neurons"]) + " Right:" + str(NN.OR.Layer["nodes"]["neurons"]) + " Expected: " + str(xor_data_trainL[i]) + ", " + str(xor_data_trainR[i]))
+
         """
         pass_test = 0
 
--- ANN/RNN_lib.py
+++ ANN/RNN_lib.py
@@ -2,97 +2,43 @@ __author__ = 'vapaspen'
 
 import numpy as np
 
-
-class RNN:
-    """
-    Class to make a test RNN
-    """
-
-    def __init__(self, layer_definitions=None):
-        self.layer_definitions = layer_definitions
-        self.Layers = {}
-
-
-class RNN_Layer:
-    """
-    Basic fully connected RNN Layer
-    """
-
-    def __init__(self, X_count, H_count):
-        self.Layer = {}
-
-        self.Layer["X_count"] = X_count
-        self.Layer["H_count"] = H_count
-
-        self.Layer["Last_input"] = np.zeros((self.Layer["X_count"],), dtype=float)
-
-        self.Layer["nodes"] = {}
-
-        self.Layer["nodes"]["neurons"] = np.zeros((self.Layer["H_count"],), dtype=float)
-        self.Layer["nodes"]["neurons_delta"] = np.zeros_like(self.Layer["nodes"]["neurons"])
-
-        self.Layer["nodes"]["bias"] = np.zeros((self.Layer["H_count"],), dtype=float)
-        self.Layer["nodes"]["bias_delta"] = np.zeros_like(self.Layer["nodes"]["bias"])
-
-        self.Layer["nodes"]["hidden_weights"] = np.random.random((self.Layer["H_count"], self.Layer["H_count"]))*0.1
-        self.Layer["nodes"]["hidden_weights_delta"] = np.zeros_like(self.Layer["nodes"]["hidden_weights"])
-
-        self.Layer["nodes"]["input_weights"] = np.random.random((self.Layer["X_count"], self.Layer["H_count"]))*0.1
-        self.Layer["nodes"]["input_weights_delta"] = np.zeros_like(self.Layer["nodes"]["input_weights"])
-
-    def feed_foward(self, input_nodes):
-        """
-        The basic feed forward Process for this layer.
-
-        :param input_nodes:
-            One dimensional Numpy array with the same length as the configured X size
-        :return:
-            Activated One dimensional numpy array
-
-        :raises:
-            Exception when input length doesnt match input settings.
-        """
-        if not len(input_nodes) == self.Layer["X_count"]:
-            raise Exception("Input given not the same shape as Layer settings.")
-
-        self.Layer["Last_input"] = input_nodes
-        self.Layer["nodes"]["neurons"] = np.tanh(np.dot(self.Layer["nodes"]["neurons"],  self.Layer["nodes"]["hidden_weights"]) + np.dot(input_nodes, self.Layer["nodes"]["input_weights"]) + self.Layer["nodes"]["bias"])
-        return self.Layer["nodes"]["neurons"]
-
-    def reset_bias(self):
-        """
-        Function to reset all of the Gradients for this layer
-        :return: void
-        """
-        self.Layer["nodes"]["neurons_delta"] = np.zeros_like(self.Layer["nodes"]["neurons"])
-        self.Layer["nodes"]["bias_delta"] = np.zeros_like(self.Layer["nodes"]["bias"])
-        self.Layer["nodes"]["input_weights_delta"] = np.zeros_like(self.Layer["nodes"]["input_weights"])
-        self.Layer["nodes"]["hidden_weights_delta"] = np.zeros_like(self.Layer["nodes"]["hidden_weights"])
-
-
 class ANN_Layer:
     """
-    Basic fully connected ANN Layer
+    ANN/RNN use is_recurrent to switch between the two.
     """
 
-    def __init__(self, X_count, H_count):
+    def __init__(self, X_count, H_count, is_recurrent=False):
         self.Layer = {}
 
         self.Layer["X_count"] = X_count
         self.Layer["H_count"] = H_count
+        self.Layer["is_recurrent"] = is_recurrent
 
         self.Layer["Last_input"] = np.zeros((self.Layer["X_count"],), dtype=float)
 
+        self.Layer["last_deltal_update"] = None
+        self.Layer["MU"] = .05
+
         self.Layer["nodes"] = {}
+
 
         self.Layer["nodes"]["neurons"] = np.zeros((self.Layer["H_count"],), dtype=float)
         self.Layer["nodes"]["neurons_delta"] = np.zeros_like(self.Layer["nodes"]["neurons"])
 
         self.Layer["nodes"]["bias"] = np.zeros((self.Layer["H_count"],), dtype=float)
         self.Layer["nodes"]["bias_delta"] = np.zeros_like(self.Layer["nodes"]["bias"])
+        self.Layer["nodes"]["bias_delta_last"] = np.zeros_like(self.Layer["nodes"]["bias"])
 
         self.Layer["nodes"]["input_weights"] = np.random.random((self.Layer["X_count"], self.Layer["H_count"]))*0.1
         self.Layer["nodes"]["input_weights_delta"] = np.zeros_like(self.Layer["nodes"]["input_weights"])
+        self.Layer["nodes"]["input_weights_delta_last"] = np.zeros_like(self.Layer["nodes"]["input_weights"])
+
+        if self.Layer["is_recurrent"]:
+            self.Layer["nodes"]["hidden_weights"] = np.random.random((self.Layer["H_count"], self.Layer["H_count"]))*0.1
+            self.Layer["nodes"]["hidden_weights_delta"] = np.zeros_like(self.Layer["nodes"]["hidden_weights"])
+            self.Layer["nodes"]["hidden_weights_delta_last"] = np.zeros_like(self.Layer["nodes"]["hidden_weights"])
+
+            self.Layer["nodes"]["last_neurons"] = np.zeros((self.Layer["H_count"],), dtype=float)
 
     def feed_foward(self, input_nodes):
         """
@@ -112,7 +58,11 @@ class ANN_Layer:
             raise Exception("Input given not the same shape as Layer settings.")
 
         self.Layer["Last_input"] = input_nodes
-        self.Layer["nodes"]["neurons"] = np.tanh(np.dot(input_nodes, self.Layer["nodes"]["input_weights"]) + self.Layer["nodes"]["bias"])
+        if self.Layer["is_recurrent"]:
+            self.Layer["nodes"]["last_neurons"] = self.Layer["nodes"]["neurons"]
+            self.Layer["nodes"]["neurons"] = np.tanh(np.dot(input_nodes, self.Layer["nodes"]["input_weights"]) + np.dot(self.Layer["nodes"]["neurons"], self.Layer["nodes"]["hidden_weights"]) + self.Layer["nodes"]["bias"])
+        else:
+            self.Layer["nodes"]["neurons"] = np.tanh(np.dot(input_nodes, self.Layer["nodes"]["input_weights"]) + self.Layer["nodes"]["bias"])
         return self.Layer["nodes"]["neurons"]
 
     def reset_bias(self):
@@ -122,7 +72,14 @@ class ANN_Layer:
         """
         self.Layer["nodes"]["neurons_delta"] = np.zeros_like(self.Layer["nodes"]["neurons"])
         self.Layer["nodes"]["bias_delta"] = np.zeros_like(self.Layer["nodes"]["bias"])
+        self.Layer["nodes"]["bias_delta_last"] = self.Layer["nodes"]["bias_delta"]
+
         self.Layer["nodes"]["input_weights_delta"] = np.zeros_like(self.Layer["nodes"]["input_weights"])
+        self.Layer["nodes"]["input_weights_delta_last"] =self.Layer["nodes"]["input_weights_delta"]
+
+        if self.Layer["is_recurrent"]:
+            self.Layer["nodes"]["hidden_weights_delta"] = np.zeros_like(self.Layer["nodes"]["hidden_weights"])
+            self.Layer["nodes"]["hidden_weights_delta_last"] = self.Layer["nodes"]["hidden_weights_delta"]
 
     def backpropagate(self, test_error, learning_rate=0.01):
         """
@@ -136,11 +93,28 @@ class ANN_Layer:
         self.Layer["nodes"]["bias_delta"] += (1 - self.Layer["nodes"]["neurons"] * self.Layer["nodes"]["neurons"]) * test_error
         self.Layer["nodes"]["input_weights_delta"] += self.Layer["nodes"]["bias_delta"] * np.array([self.Layer["Last_input"]]).T
 
-        #Update all Layer Parameters at once.
-        for param, delta_param in zip([self.Layer["nodes"]["bias"], self.Layer["nodes"]["input_weights"]],
-                                      [self.Layer["nodes"]["bias_delta"], self.Layer["nodes"]["input_weights_delta"]]):
-            np.clip(delta_param, -10, 10, out=delta_param)
-            param += -learning_rate * delta_param
+
+        if not self.Layer["is_recurrent"]:
+
+            #Update all Layer Parameters at once.
+            for param, delta_param in zip([self.Layer["nodes"]["bias"], self.Layer["nodes"]["input_weights"]],
+                                          [self.Layer["nodes"]["bias_delta"], self.Layer["nodes"]["input_weights_delta"]]):
+                np.clip(delta_param, -10, 10, out=delta_param)
+                param += -learning_rate * delta_param
+
+        else:
+            self.Layer["nodes"]["hidden_weights_delta"] += self.Layer["nodes"]["bias_delta"] * np.array([self.Layer["nodes"]["last_neurons"]]).T
+
+            #Update all Layer Parameters at once.
+            for param, delta_param, delta_last in zip([self.Layer["nodes"]["bias"], self.Layer["nodes"]["input_weights"], self.Layer["nodes"]["hidden_weights"]],
+                                          [self.Layer["nodes"]["bias_delta"], self.Layer["nodes"]["input_weights_delta"], self.Layer["nodes"]["hidden_weights_delta"]],
+                                           [self.Layer["nodes"]["bias_delta_last"], self.Layer["nodes"]["input_weights_delta_last"], self.Layer["nodes"]["hidden_weights_delta_last"]]):
+                np.clip(delta_param, -10, 10, out=delta_param)
+
+
+
+                param -= (learning_rate * delta_param) + (delta_last * self.Layer["MU"])
+
 
         return np.dot(self.Layer["nodes"]["bias_delta"], self.Layer["nodes"]["input_weights"].T)
 
